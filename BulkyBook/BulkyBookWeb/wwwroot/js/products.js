d52f7ff4207931c1f4e1c91fd27d693d7283ad42
dsamaddar <debumist@gmail.com>
2026-08-27 13:18:02 +0600

    upto lecture 87

--- BulkyBook/BulkyBookWeb/wwwroot/js/products.js
+++ BulkyBook/BulkyBookWeb/wwwroot/js/products.js
@@ -2,22 +2,19 @@
 var productDataTable;
 
 $(document).ready(function () {
-    productDataTable();
-});
-
-productDataTable = $('#tblData').DataTable({
-    ajax: '/admin/product/getall',
-    columns: [
-        { data: 'title', "width": "25%" },
-        { data: 'isbn', "width": "15%" },
-        { data: 'price', "width": "10%", "render": function (data) { return '৳ ' + data.toFixed(2); } },
-        { data: 'author', "width": "15%" },
-        {
-            data: 'category.name', "width": "10%", "render": function (data) { return '<span class="badge bg-secondary">' + data + '</span>'; }
-        },
-        {
-            data: 'id', "width": "25%", "render": function (data) {
-                return `<div class="d-flex gap-2 justity-content-end">
+    productDataTable = $('#tblData').DataTable({
+        ajax: '/admin/product/getall',
+        columns: [
+            { data: 'title', "width": "25%" },
+            { data: 'isbn', "width": "15%" },
+            { data: 'price', "width": "10%", "render": function (data) { return '৳ ' + data.toFixed(2); } },
+            { data: 'author', "width": "15%" },
+            {
+                data: 'category.name', "width": "10%", "render": function (data) { return '<span class="badge bg-secondary">' + data + '</span>'; }
+            },
+            {
+                data: 'id', "width": "25%", "render": function (data) {
+                    return `<div class="d-flex gap-2 justify-content-end">
                     <a href="/admin/product/upsert?id=${data}" class="btn btn-sm btn-outline-success">
                         <i class="bi bi-pencil-square"></i> Edit
                     </a>
@@ -25,10 +22,13 @@ productDataTable = $('#tblData').DataTable({
                         <i class="bi bi-trash"></i> Delete
                     </a>
                 </div> `;
+                }
             }
-        }
-    ]
+        ]
+    });
 });
+
+
 
 function Delete(url) {
     Swal.fire({
